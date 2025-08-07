b63c43af43f77a7751888932c238209fc4a33546
bezerra9 <gabrielbezerragof@gmail.com>
2025-08-07 11:12:04 -0300

    commit-adicionando-novas-funcionalidades-e-modularização-do-js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+initScrollSuave();
+
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+initAnimacaoScroll();
+
+import initAccordion from "./modules/accordion.js";
+initAccordion();
+
+import initTabNav from "./modules/tab.js";
+initTabNav();
+
+import initModal from "./modules/modal.js";
+initModal();
+
+import initTooltip from "./modules/tooltip.js";
+initTooltip()
